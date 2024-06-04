e98b26ee3a0358d710be880cfe29923abcb915d6
pykroth <145720162+pykroth@users.noreply.github.com.>
2024-06-04 16:56:55 -0500

    fade in animation for classic button

--- dle/src/routes/+page.svelte
+++ dle/src/routes/+page.svelte
@@ -29,7 +29,7 @@
 	<div class="flex flex-col items-center justify-center flex-grow">
 		<div class="my-8">
 			<a href="/Classic">
-				<button class="border border-black text-white font-bold py-2 px-4 rounded bg-gray-800 hover:bg-gray-700 transition-colors duration-200">
+				<button id="classic" class="border border-black text-white font-bold py-2 px-4 rounded bg-gray-800 hover:bg-gray-700 transition-colors duration-200">
 					Classic Button
 				</button>
 			</a>
@@ -40,5 +40,34 @@
 <style lang="postcss">
 	#Title-Screen {
 		font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+
+	@keyframes fade-up
+	{
+		0%{
+			opacity:0;
+			transform:translateY(30px) scale(.9);
+		}
+		100%
+		{
+			opacity:1;
+			transform:translateY(0px) scale(1);
+		}
+	}
+	@keyframes fade-up
+	{
+		0%{
+			opacity:0;
+			transform:translateY(-30px) scale(.9);
+		}
+		100%
+		{
+			opacity:1;
+			transform:translateY(0px) scale(1);
+		}
+
+	}
+	#classic{
+		animation:fade-up 2s;
 	}
 </style>
